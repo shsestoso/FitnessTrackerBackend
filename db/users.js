0af91523102c16c0b545be347d6656fb9945e1bb
An Nguyen <annguyen@Ans-MacBook-Pro.lan>
2023-01-30 10:24:16 -0600

    update

--- db/users.js
+++ db/users.js
@@ -4,7 +4,19 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
-  
+  try {
+    const {rows: [user]} = await client.query(`
+    INSERT INTO users(username, password)
+    VALUES($1, $2)
+    ON CONFLICT (username) DO NOTHING
+    RETURNING *;
+    `,[username, password]);
+    return user;
+  }
+  catch(error){
+    console.error('Error creating users!');
+    throw error;
+  }
 }
 
 async function getUser({ username, password }) {
