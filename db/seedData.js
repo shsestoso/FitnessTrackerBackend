712685816d9fab67873c39a35b489b00687d9ceb
Shaira grace sestoso <shairagracesestoso@Shairas-Air.attlocal.net>
2023-02-01 08:33:20 -0600

    added table context on seedData.js

--- db/seedData.js
+++ db/seedData.js
@@ -10,7 +10,9 @@ async function dropTables() {
   try{
   console.log("Dropping All Tables...")
   await client.query(`
-  DROP TABLE IF EXISTS mytablename;
+  DROP TABLE IF EXISTS users;
+  DROP TABLE IF EXISTS activities;
+  DROP TABLE IF EXISTS routines;
   `);
   }
   catch (error){
@@ -23,7 +25,25 @@ async function createTables() {
   try {
     console.log("Starting to build tables...");
     await client.query(`
-    CREATE TABLE mytablename
+    CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR (255) NOT NULL
+    );
+
+    CREATE TABLE activities(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      description TEXT NOT NULL
+    );
+
+    CREATE TABLE routines(
+      id SERIAL PRIMARY KEY,
+      "creatorId" INTEGER REFERENCES users(id),
+      "isPublic" BOOLEAN DEFAULT false,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      goal TEXT NOT NULL
+    );
     `)
   } catch (error) {
     console.error("Error building tables!");
