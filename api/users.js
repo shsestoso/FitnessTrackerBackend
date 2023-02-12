e4207edd977aebd0d548b876f85c702ea6b6cb4a
An Nguyen <annguyen@Ans-MacBook-Pro.lan>
2023-02-12 08:07:05 -0600

    update

--- api/users.js
+++ api/users.js
@@ -1,8 +1,26 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
 const router = express.Router();
+const {createUser, getUser} = require('../db');
+const jwt = require('jsonwebtoken');
+const {JWT} = process.env;
+
+
 
 // POST /api/users/register
+router.post('/register', async(req, res, next)=>{
+    try{
+        const {username, password} = req.body;
+        if (password.length < 8){
+            throw new Error ("Password length need to be at least 8 character");
+        }
+        const user = await createUser({username, password});
+        res.send({user});
+    }
+catch(err){
+next(err);
+}
+});
 
 // POST /api/users/login
 
--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
+// app.get('/products/:id', function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for all origins!'})
+// })
+
+// app.listen(80, function () {
+//   console.log('CORS-enabled web server listening on port 80')
+// })
 
 // Setup your Middleware and API Router here
 
