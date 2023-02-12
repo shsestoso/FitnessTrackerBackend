ab1abd61a8e8c0cbb254bff0a3aaf2be1d9bb8db
Shaira grace sestoso <shairagracesestoso@Shairas-Air.attlocal.net>
2023-02-12 08:23:12 -0600

    practice

--- api/users.js
+++ api/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {createUser, getUser} = require('../db');
 const jwt = require('jsonwebtoken');
 const {JWT} = process.env;
-
+// practice 
 
 
 // POST /api/users/register
