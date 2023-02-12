653c118815b50c45fd8f32560914f9e5e7195bc1
An Nguyen <annguyen@Ans-MacBook-Pro.lan>
2023-02-12 13:46:40 -0600

    update

--- api/routines.js
+++ api/routines.js
@@ -1,13 +1,99 @@
 const express = require('express');
+const { getRoutineActivitiesByRoutine, createRoutine, getAllPublicRoutines, updateRoutine, destroyRoutine, destroyRoutineActivity } = require('../db');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = process.env;
+
+
 
 // GET /api/routines
 
+router.get('/', async (req, res, next) => {
+
+    const routines = await getAllPublicRoutines();
+    res.send(routines);
+
+});
+
 // POST /api/routines
+router.post('/', async (req, res, next)=>{
+    const {isPublic, name, goal} = req.body;
+    const authorization = req.header("authorization");
+    try {
+        if(!authorization){
+            res.send({
+            error: "Error",
+            message: "You must be logged in to perform this action",
+            name: "Error"
+            });        
+        }
+        else{
+            const token = authorization.slice(7)
+            const user = jwt.verify(token, process.env.JWT_SECRET);
+            const creatorId = user.id;
+            const newRoutine = await createRoutine({creatorId, isPublic, name, goal})
+            res.send(newRoutine);
+        }
+    } catch (error) {
+        next(error)
+    }
+})
 
 // PATCH /api/routines/:routineId
 
+router.patch('/:routineId', async(req, res, next)=>{
+        const authorization = req.header("authorization");
+    try {
+        if(!authorization){
+            res.send({
+            error: "Error",
+            message: "You must be logged in to perform this action",
+            name: "Error"
+            });        
+        }
+        else{
+        const {isPublic, name, goal} = req.body;
+        const routine = await updateRoutine({
+            id: req.params.routineId,
+            isPublic,
+            name,
+            goal
+        });
+        res.send(routine);
+    }
+}
+    catch(error){
+        next(error);
+    }
+})
+
 // DELETE /api/routines/:routineId
+
+router.delete('/:routineId', async(req, res, next) =>{
+    const authorization = req.header("authorization");
+    try{
+        if(!authorization){
+            res.send({
+            error: "Error",
+            message: "You must be logged in to perform this action",
+            name: "Error"
+            });        
+        }
+        else{
+        const routineActivities = await getRoutineActivitiesByRoutine({
+            routineId: req.params.routineId,
+        });
+        for (let i = 0; i < routineActivities.length; i++){
+            await destroyRoutineActivity(routineActivities[i].id);
+        }
+        const deleteRoutines = await destroyRoutine( {id: req.params.routineId});
+        res.send(deleteRoutines);
+        }
+    }
+    catch(error){
+        next(error);
+    }
+} )
 
 // POST /api/routines/:routineId/activities
 
--- api/users.js
+++ api/users.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
 const userRouter = express.Router();
-const {createUser, getUserByUsername, getUser} = require('../db');
+const {createUser, getUserByUsername, getUser, getPublicRoutinesByUser, getAllRoutinesByUser} = require('../db');
 const jwt = require('jsonwebtoken');
 const {JWT_SECRET} = process.env;
 
@@ -15,17 +15,19 @@ userRouter.use((req, res, next) => {
 // POST /api/users/register
 userRouter.post('/register', async(req, res, next)=>{
     const {username, password} = req.body;
-    
+
     try{
        const users = await getUserByUsername(username);
        if (users){
         res.status(500).send({name: "AN", message: `User ${username} is already taken.`, error: "failed"});
        }
         else if (password.length < 8){
-            res.status(500).send({name: "Shaira", message: `Password Too Short!`, error: "it's failed"});        }
-        const user = await createUser({username, password});
-        const token = jwt.sign({id: user.id, username}, JWT_SECRET, {expiresIn: "1w"})
+            res.status(500).send({name: "Shaira", message: `Password Too Short!`, error: "it's failed"});}
+        else{
+            const user = await createUser({username, password});
+        const token = jwt.sign({id: user.id, username: user.username}, process.env.JWT_SECRET)
         res.send({message: "thank you for signing up", token, user})
+        }
     }
 catch ({name, message, error}){
 next(name, message, error);
@@ -38,32 +40,64 @@ userRouter.post('/login', async (req, res, next)=>{
     try {
         const{username, password} = req.body;
         const user = await getUser({username,password});
-        if (user){
+        const Username = await getUserByUsername(username)
+        if (!user){
             res.status(401).send({name:"", message:"", error:""});
         }
+        else{
         const token = jwt.sign(
             {id: user.id, username: user.username}, 
-            JWT_SECRET
+            process.env.JWT_SECRET
         );
-        res.send({token});
+        res.send({token,message: "you're logged in!",user: {id: Username.id, username}});
+        }
     } catch (err) {
         next(err);
     }
 })
 
 // GET /api/users/me
-// userRouter.get('/me', async (req, res, next)=> {
-//    const {username} = req.body;
-//    try {
-//     const users = getUser(username);
-//     if (users){
-//         res.status(401).send({name: })
-//     }
-//    } catch (error) {
-    
-//    }
-// })
+userRouter.get('/me', async (req, res, next)=> {
+   try {
+    const authorization = req.header("Authorization");
+    console.log(authorization)
+
+    if (!authorization){
+        res.status(401).send({error: "Access denied. No token provided.",
+     message: "You must be logged in to perform this action",
+    name: "Error"});
+    }
+    else{
+        const token = authorization.slice(7)
+        const toverify = jwt.verify(token, process.env.JWT_SECRET);
+        const username = toverify.username 
+        const user = await getUserByUsername(username);
+        if (token){
+            res.send(user);
+        }
+    }
+   } catch (error) {
+    next(error);
+   }
+})
 
 // // GET /api/users/:username/routines
+userRouter.get('/:username/routines', async (req,res, next) =>{
+    const {username} = req.params;
+    const token = req.header("Authorization").slice(7);
+    const toverify = jwt.verify(token, process.env.JWT_SECRET);
+    const routines = await getPublicRoutinesByUser({username})
+    const allRoutines = await getAllRoutinesByUser({username})
+    try {
+        if(toverify.username === username){
+            res.send(allRoutines)
+        }
+        else{
+            res.send(routines)
+        }
+    } catch (error) {
+        next(error);
+    }
+})
 
 module.exports = userRouter;
