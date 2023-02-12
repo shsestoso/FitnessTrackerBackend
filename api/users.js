31e946fffe4a7303c262e8f196331070b4f4dacb
Shaira grace sestoso <shairagracesestoso@Shairas-Air.attlocal.net>
2023-02-12 09:02:18 -0600

    trying out codes for api

--- api/users.js
+++ api/users.js
@@ -3,13 +3,18 @@ const express = require("express");
 const userRouter = express.Router();
 const {createUser} = require('../db');
 
-// practice 
 
+userRouter.use((req, res, next) => {
+    console.log('a request is being made');
+    next ();
+})
 
 // POST /api/users/register
 userRouter.post('/register', async(req, res, next)=>{
+    const {username, password} = req.body;
+    
     try{
-        const {username, password} = req.body;
+       
         if (password.length < 8){
             throw new Error ("Password length need to be at least 8 character");
         }
