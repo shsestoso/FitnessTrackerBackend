0add657d2842817a1cdf828f103e034a6eae3e48
Shaira grace sestoso <shairagracesestoso@Shairas-Air.attlocal.net>
2023-02-12 22:05:54 -0600

    added more code to activities api

--- api/activities.js
+++ api/activities.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const { createActivity } = require('../db');
 const router = express.Router();
-const {getPublicRoutinesByActivity, getAllActivities} = ('../db')
+const {getPublicRoutinesByActivity, getAllActivities, updateActivity} = ('../db')
 
 // GET /api/activities/:activityId/routines
 router.get("/:activityId/routines", async(req, res, next) => {
@@ -17,11 +18,49 @@ router.get("/:activityId/routines", async(req, res, next) => {
 })
 // GET /api/activities
 router.get('/', async(req, res, next) => {
-   const activities = await getAllActivities();
-   res.send(activities);
+ try {
+    const activities = await getAllActivities();
+ res.send(activities)
+ } catch (error) {
+    next(error)
+ }
 })
 
 // POST /api/activities
+router.post('/', async(req, res, next) => {
+    console.log('this is post:', req.body)
+    const {name, description} = req.body;
+    try {
+        const activities = await createActivity({name, description});
+        res.send(activities)
+    } catch (error) {
+        next(error)
+    }
+})
 
 // PATCH /api/activities/:activityId
+router.patch('/:activityId', async(req, res, next) => {
+    console.log('this is params for patch:', req.params);
+    console.log('this is body for patch', req.body)
+    const {activityId} = req.params;
+    const {name, description} = req.body;
+
+    const updateFields = {};
+
+    if(name){
+        updateFields.name = name;
+    }
+    if (description){
+        updateFields.description = description;
+    }
+    try {
+        const updatingActivities = await updateActivity({activityId})
+        if(!updatingActivities.length) {
+            res.send({error: "Error updating activities", message: " ", name: "Sean"})
+        }
+        res.send(updatingActivities)
+    } catch (error) {
+        next(error)
+    }
+})
 module.exports = router;
